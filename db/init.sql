d2ca14d407612dc831ef833c5cbb2d960ce1fb5a
Dominus-Exitio <104685511+Dominus-Exitio@users.noreply.github.com>
2025-10-23 20:18:03 +0300

    Update init.sql
    Переделана модель базы данных.
    Вероятна требует доработки с учетом мнения других участников

--- db/init.sql
+++ db/init.sql
@@ -1,69 +1,63 @@
 -- Создание таблицы для студентов
 
 CREATE TABLE student (
-    id UUID PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    surname VARCHAR(100) NOT NULL,
-    middle_name VARCHAR(100),
-    card_number BIGINT UNIQUE NOT NULL,
-    faculty VARCHAR(50) NOT NULL,
-    university VARCHAR(100) NOT NULL,
-    department VARCHAR(100),
-    study_group VARCHAR(50),
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    description VARCHAR(256)
+    id UUID PRIMARY KEY, -- Уникальный идентификатор студента
+    name VARCHAR(50) NOT NULL, -- Имя студента
+    surname VARCHAR(50) NOT NULL, -- Фамилия студента
+    middle_name VARCHAR(50), -- Отчество студента
+    card_number BIGINT UNIQUE NOT NULL, -- Номер студенческого билета
+    faculty VARCHAR(50) NOT NULL, -- Факультет/институт студента
+    university VARCHAR(100) NOT NULL, -- Университет студента
+    department VARCHAR(100), -- Кафедра студента
+    study_group VARCHAR(50), -- Учебная группа студента
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата и время создания записи
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата и время последнего обновления записи
 );
 
 -- Создание таблицы для конкурсов
 
 CREATE TABLE competition (
-    id UUID PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    organizer VARCHAR(256),
-    type VARCHAR(50) NOT NULL CHECK (type IN ('OLYMPIAD', 'CONTEST', 'CHAMPIONSHIP', 'COMPETITION')),
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    description VARCHAR(256)
-);
-
--- Создание таблицы для уровня
-
-CREATE TABLE level (
-    id BIGINT PRIMARY KEY,
-    code VARCHAR(100) UNIQUE NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    description VARCHAR(256)
-);
-
--- Создание таблицы для результатов
-
-CREATE TABLE result (
-    id BIGINT PRIMARY KEY,
-    code VARCHAR(256) UNIQUE NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    description VARCHAR(256)
+    id UUID PRIMARY KEY, -- Уникальный идентификатор конкурса
+    name VARCHAR(100) NOT NULL, -- Название конкурса/олимпиады
+    organizer VARCHAR(256), -- Организатор конкурса
+    type VARCHAR(50) NOT NULL CHECK (type IN ('OLYMPIAD', 'CONTEST', 'CHAMPIONSHIP', 'COMPETITION')), -- Тип мероприятия 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата и время создания записи
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата и время последнего обновления записи
+    
 );
 
 -- Создание таблицы для участия
 
 CREATE TABLE participation (
-    id UUID PRIMARY KEY,
-    student_id UUID NOT NULL REFERENCES student(id),
-    competition_id UUID NOT NULL REFERENCES competition(id),
-    level_id BIGINT NOT NULL REFERENCES level(id),
-    result_id BIGINT NOT NULL REFERENCES result(id),
-    year INTEGER NOT NULL CHECK (year >= 2000 AND year <= EXTRACT(YEAR FROM CURRENT_DATE) + 1),
-    supervisor VARCHAR(256),
-    is_blocked BOOLEAN NOT NULL,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    description VARCHAR(256),
+    id UUID PRIMARY KEY, -- Уникальный идентификатор участия
+    student_id UUID NOT NULL REFERENCES student(id), -- Ссылка на студента 
+    competition_id UUID NOT NULL REFERENCES competition(id), -- - Ссылка на конкурс
+	points NUMERIC(6, 2) NOT NULL, -- Количество баллов
+    level VARCHAR(20) NOT NULL CHECK (level IN  ('Вузовский', 'Региональный', 'Всероссийский', 'Международный')), -- Уровень участия
+    result VARCHAR(20) NOT NULL CHECK( result IN ('Победитель', 'Призер', 'Участник', 'Дисквалифицирован', 'Дипломант')), -- Результат участия
+    year INTEGER NOT NULL CHECK (year >= 2000 AND year <= EXTRACT(YEAR FROM CURRENT_DATE) + 1), -- Год участия
+    supervisor VARCHAR(256), -- ФИО научного руководителя/куратора 
+    is_blocked BOOLEAN NOT NULL, -- Флаг блокировки редактирования записи 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата и время создания записи (автоматически)
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Дата и время последнего обновления записи (автоматически)
+    description TEXT, -- Комментарий к участию
 
     -- Уникальное ограничение для предотвращения дубликатов
     UNIQUE(student_id, competition_id, year)
+);
+
+-- Создание таблицы для пользователей
+
+CREATE TABLE users (
+	login VARCHAR(20) PRIMARY KEY, -- Логин
+	password VARCHAR(20), -- Пароль
+	name VARCHAR(20) NOT NULL, -- Имя
+	surname VARCHAR(30) NOT NULL, -- Фамилия
+	middle_name VARCHAR(20), -- Отчетво
+	organization VARCHAR(100), -- Организатор
+	level VARCHAR(20) CHECK(level IN ('Вузовский', 'Региональный', 'Всероссийский', 'Международный')), -- Уровень
+	role VARCHAR(20) NOT NULL CHECK(role IN ('organizer', 'admin', 'su')), -- Роль
+	CONSTRAINT chk_level_for_organizer CHECK (
+		(role != 'organizer') OR (level IS NOT NULL)
+	)
 );
