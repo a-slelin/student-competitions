d681cd82f6d212675228a3bb4ed2908d24e4f9d5
Alexandr Slelin <a.slelin.work@mail.ru>
2025-11-10 17:06:46 +0300

    update: participation entity
    Обновил сущность участия. В sql добавил валидацию.
    В sql поменял ON DELETE RESTRICT, так как при удалении участия не должны удаляться студенты или соревнования.

--- backend/src/main/resources/db/changelog/changes/005-create-table-participation.sql
+++ backend/src/main/resources/db/changelog/changes/005-create-table-participation.sql
@@ -8,18 +8,21 @@ CREATE TABLE participation (
     competition_id UUID NOT NULL,
     level_code VARCHAR(20) NOT NULL,
     result_code VARCHAR(20) NOT NULL,
-    year INTEGER NOT NULL,
+    year INTEGER NOT NULL
+        CHECK(year >= 0),
     supervisor VARCHAR(100),
-    points INTEGER,
-    description TEXT,
+    points INTEGER
+        CHECK (points IS NULL OR points >= 0),
+    description TEXT
+        CHECK (description IS NULL OR LENGTH(TRIM(description)) > 0),
     is_blocked BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_participation_student FOREIGN KEY (student_id)
-        REFERENCES student(id) ON DELETE CASCADE,
+        REFERENCES student(id) ON DELETE RESTRICT,
     CONSTRAINT fk_participation_competition FOREIGN KEY (competition_id)
-        REFERENCES competition(id) ON DELETE CASCADE,
+        REFERENCES competition(id) ON DELETE RESTRICT,
     CONSTRAINT fk_participation_level FOREIGN KEY (level_code)
         REFERENCES level(code) ON DELETE RESTRICT,
     CONSTRAINT fk_participation_result FOREIGN KEY (result_code)
