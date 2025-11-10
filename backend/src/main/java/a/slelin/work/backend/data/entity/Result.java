575956901977e80011774c33469476547451f013
Alexandr Slelin <a.slelin.work@mail.ru>
2025-11-10 12:53:18 +0300

    add: result entity
    Добавил базовую версию сущности результат.

---
+++ backend/src/main/java/a/slelin/work/backend/data/entity/Result.java
@@ -0,0 +1,58 @@
+package a.slelin.work.backend.data.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity(name = Result.ENTITY_NAME)
+@Table(name = Result.TABLE_NAME)
+public class Result {
+
+    // Static fields
+
+    public static final String ENTITY_NAME = "result";
+
+    public static final String TABLE_NAME = "result";
+
+    // Fields
+
+    @Id
+    @Column(length = 20)
+    @Size(min = 3, max = 20)
+    @NotNull
+    @Pattern(regexp = "^[A-Z]+$")
+    private String code;
+
+    @Column(unique = true,
+            nullable = false,
+            length = 50)
+    @Size(min = 3, max = 50)
+    @NotBlank
+    private String name;
+
+    @Column(nullable = false, name = "sort_order")
+    @NotNull
+    @Min(0)
+    private Integer sortOrder;
+
+    // Methods
+
+    @Override
+    public String toString() {
+        return """
+                
+                Result:
+                    code = %s,
+                    name = %s,
+                    sortOrder = %d.
+                
+                """.formatted(code, name, sortOrder);
+    }
+}
