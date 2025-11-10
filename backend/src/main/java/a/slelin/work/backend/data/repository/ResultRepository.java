ed7f329fabffead99972cb69f8d835a690d66809
Alexandr Slelin <a.slelin.work@mail.ru>
2025-11-10 22:07:58 +0300

    add: result service
    Добавил сервис для результатов.

--- backend/src/main/java/a/slelin/work/backend/data/repository/ResultRepository.java
+++ backend/src/main/java/a/slelin/work/backend/data/repository/ResultRepository.java
@@ -1,6 +1,8 @@
 package a.slelin.work.backend.data.repository;
 
 import a.slelin.work.backend.data.entity.Result;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -9,4 +11,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ResultRepository extends JpaRepository<Result, String>,
         PagingAndSortingRepository<Result, String>, JpaSpecificationExecutor<Result> {
+
+    Page<Result> findAllBySortOrder(Integer sortOrder, Pageable pageable);
+
+    Page<Result> findAllByNameContaining(String name, Pageable pageable);
+
+    Page<Result> findAllBySortOrderAndNameContaining(Integer sortOrder, String name, Pageable pageable);
+
+    boolean existsByName(String name);
+
+    boolean existsByNameAndIdNot(String name, String id);
 }
